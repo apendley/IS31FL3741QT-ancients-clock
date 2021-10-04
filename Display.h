1cb086bcddce21a1ac5a1149f9d8e972f8b97812
Aaron Pendley <aaronpendley@gmail.com>
2021-10-04 09:54:59 -0500

    Reduce global current even further; this thing is still really bright.

--- Display.h
+++ Display.h
@@ -54,7 +54,7 @@ public:
             matrix->setLEDscaling(255);
             // Tone down the brightness so we don't overload the regulator.
             // Also because these things are blisteringly bright.
-            matrix->setGlobalCurrent(16);
+            matrix->setGlobalCurrent(8);
             matrix->enable(true);
             matrix->setRotation(0);
             matrix->setTextWrap(false);            
